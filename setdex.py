5788a4d6e7818dc513ca3f25820a8bb2d2f4bfc0
Simonif <dunsparce@web.de>
2024-08-21 23:22:58 +0200

    crazy anas zeug

--- setdex.py
+++ setdex.py
@@ -5,22 +5,40 @@
 import json
  
 # Opening JSON file
-calc = open('calc.json')
+calc = open('calc.json', encoding="utf8")
  
 # returns JSON object as 
 # a dictionary
 data = json.load(calc)
 
+# list of ignored trainer numbers
+ignored_trainers = []
+
 mon_store = {}
+party_order = {}
 for trainer in data:
-  for mon_name in data[trainer].keys():
-    mon_name_actual = mon_name[:-2]
+  trainer_num = int(trainer[-3:])
+  trainer_name = trainer[:-4]
+  if trainer_num in ignored_trainers: continue
+  final_party = []
+  clean_party = [x[:-2] for x in data[trainer].keys()]
+  for mon_name in clean_party:
+    if len([x for x in clean_party if x == mon_name]) > 1:
+      i = 1
+      while f'{mon_name} ({i})' in final_party:
+        i = i + 1
+      final_party.append(f'{mon_name} ({i})')
+    else: final_party.append(mon_name)
+  party_order[trainer_name + f' [#{trainer_num}]'] = final_party
+  
+  for mon_name in final_party:
+    mon_index = final_party.index(mon_name)
+    dirty_mon_name = list(data[trainer].keys())[mon_index]
+    mon_name_actual = mon_name.split(' (')[0]
     if mon_name_actual not in mon_store:
       mon_store[mon_name_actual] = {}
-    if trainer[-3:-1] == '00':
-      mon_store[mon_name_actual][trainer[:-4] + ' (Trainer #' + trainer[-1:] + ' Mon #' + mon_name[-1:] + ')'] = data[trainer][mon_name]
-    elif trainer[-3:-2] == '0':
-      mon_store[mon_name_actual][trainer[:-4] + ' (Trainer #' + trainer[-2:] + ' Mon #' + mon_name[-1:] + ')'] = data[trainer][mon_name]
-    else:
-      mon_store[mon_name_actual][trainer[:-4] + ' (Trainer #' + trainer[-3:] + ' Mon #' + mon_name[-1:] + ')'] = data[trainer][mon_name]
-json.dump(mon_store, open('setdex.json', 'w'), sort_keys=True, indent='\t', separators=(',', ': '))
+    if "(" in mon_name: mon_store[mon_name_actual][trainer_name + ' ' + mon_name.split(" ")[-1] + f' [#{trainer_num}]'] = data[trainer][dirty_mon_name]
+    else: mon_store[mon_name_actual][trainer_name + f' [#{trainer_num}]'] = data[trainer][dirty_mon_name]
+
+json.dump(mon_store, open('setdex.json', 'w', encoding="utf8"), sort_keys=True, indent='\t', separators=(',', ': '), ensure_ascii=False)
+json.dump(party_order, open('party_order.json', 'w', encoding="utf8"), indent='\t', separators=(',', ': '), ensure_ascii=False)
